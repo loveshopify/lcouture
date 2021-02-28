24c47825f253e5fff0eca1c9b74b60717f5b8499
unknown <love.shopify25@gmail.com>
2021-02-28 15:51:20 -0800

    update annoucement

--- assets/custom.js
+++ assets/custom.js
@@ -22,6 +22,21 @@
  *   bubbles: true
  * }));
  */
+// Announcement Bar
+var backButton = '<span class="slick-prev">&#8592;</span>';
+var nextButton = '<span class="slick-next">&#8594;</span>';
+$("[data-announcement-slider]").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: false,
+  infinite: true,
+  prevArrow: backButton,
+  nextArrow: nextButton
+})
+
+
+
 if(template == 'collection') {
   $(document).on("click", '.Popover__ValueList .Popover__Value', function(){
     $(".CollectionToolbar__Item span").text($(this).text());
@@ -78,7 +93,6 @@ if(template == 'product') {
   })
 }
 
-
 if(template == "index") {
     // $(".shopify-section--slideshow").find(".flickity-prev-next-button.next").html('<?xml version="1.0" encoding="iso-8859-1"?><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path d="M506.134,241.843c-0.006-0.006-0.011-0.013-0.018-0.019l-104.504-104c-7.829-7.791-20.492-7.762-28.285,0.068c-7.792,7.829-7.762,20.492,0.067,28.284L443.558,236H20c-11.046,0-20,8.954-20,20c0,11.046,8.954,20,20,20h423.557l-70.162,69.824c-7.829,7.792-7.859,20.455-0.067,28.284c7.793,7.831,20.457,7.858,28.285,0.068l104.504-104c0.006-0.006,0.011-0.013,0.018-0.019C513.968,262.339,513.943,249.635,506.134,241.843z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>');
     // $(".shopify-section--slideshow").find(".flickity-prev-next-button.previous").html('<?xml version="1.0" encoding="iso-8859-1"?><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path d="M492,236H68.442l70.164-69.824c7.829-7.792,7.859-20.455,0.067-28.284c-7.792-7.83-20.456-7.859-28.285-0.068l-104.504,104c-0.007,0.006-0.012,0.013-0.018,0.019c-7.809,7.792-7.834,20.496-0.002,28.314c0.007,0.006,0.012,0.013,0.018,0.019l104.504,104c7.828,7.79,20.492,7.763,28.285-0.068c7.792-7.829,7.762-20.492-0.067-28.284L68.442,276H492c11.046,0,20-8.954,20-20C512,244.954,503.046,236,492,236z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>');    
@@ -86,8 +100,6 @@ if(template == "index") {
     $(".shopify-section--slideshow").find(".flickity-prev-next-button.previous").html('&#8592');
     $(".ProductList--carousel").find(".flickity-prev-next-button.next").html('&#8594');
     $(".ProductList--carousel").find(".flickity-prev-next-button.previous").html('&#8592');
-    var backButton = '<span class="slick-prev">&#8592;</span>';
-    var nextButton = '<span class="slick-next">&#8594;</span>';
 
     $('[data-collection-leaset-slider]').slick({
         slidesToShow: 5,
@@ -107,11 +119,6 @@ if(template == "index") {
             }
           }]
     })
-    setTimeout(function(){
-        $('[data-collection-leaset-slider]').slick("next");
-        // $('[data-collection-leaset-slider] .slick-track').css("transform", "translate3d(0px, 0px, 0px)");
-        console.log($('[data-collection-leaset-slider]').find(".slick-track").css('transform'));
-    }, 10)
     
     $('[data-article--list]').slick({
         slidesToShow: 2,
