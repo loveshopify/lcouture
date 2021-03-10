d13cdfe4711edaa21fcf8d0bc34225b6f92133dd
unknown <love.shopify25@gmail.com>
2021-03-10 00:48:48 -0800

    update theme

--- assets/custom.js
+++ assets/custom.js
@@ -28,7 +28,7 @@ var nextButton = '<span class="slick-next">&#8594;</span>';
 $("[data-announcement-slider]").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true,
+  arrows: false,
   dots: false,
   infinite: true,
   prevArrow: backButton,
@@ -68,6 +68,9 @@ if(template == 'product') {
     var product_size_value = newSizeChart[1];
     $(".main__title").text(product_size_name[0]);
     $(".sizeChart__title").text(product_size_value[0]);
+    $(".sizeChart__title_group").html("");
+    $(".size__name").html("");
+    $(".size__value").html("");
     for(var i = 1; i < newSizeChart.length; i++) $(".sizeChart__title_group").append("<li>" + newSizeChart[i][0] + "</li>");
     for(var i = 1; i < product_size_name.length; i++) $(".size__name").append("<li>" + product_size_name[i] + "</li>");
     for(var i = 1; i < product_size_value.length; i++) $(".size__value").append("<li>" + product_size_value[i] + "</li>");
@@ -99,22 +102,49 @@ if(template == 'product') {
     $(".PageOverlay").removeClass("is-visible");
   })
 
-  $(document).on("click", ".ColorSwatch", function(event){
-    if($(window).width() > 640) {
-      // event.preventDefault();
-      var _class = $(this).data('tooltip');
-      _class = "." + _class;
-      if($(this).parents(".Product__Wrapper").find(".product__media").find(_class).length != 0) {
-          $(this).parents(".Product__Wrapper").find(".product__media").find(".variant-item").hide();
-          $(this).parents(".Product__Wrapper").find(".product__media").find(_class).show();
+  $(document).on("click", ".ColorSwatchList .HorizontalList__Item:not(.active)", function(event){
+    _this = $(this);
+    $(this).closest(".HorizontalList").find(".HorizontalList__Item").removeClass("active");
+    $(this).addClass("active");
+    $.ajax($(this).attr("href"), {
+      success: function(data) {
+        var productHandle = _this.attr("href").replace('/products/', '');
+        console.log($(data).find("#judgeme_product_reviews").data("id"));
+        _this.closest(".Product__Wrapper").find(".Product__Gallery").html($(data).find(".Product__Gallery").html());
+        _this.closest(".Product.Product--small").find(".Product__OffScreen").html($(data).find(".Product__OffScreen").html());
+        _this.closest(".Product__InfoWrapper").html($(data).find(".Product__InfoWrapper").html());
+        $("#judgeme_product_reviews").html($(data).find("#judgeme_product_reviews").html());
+        // console.log($(data).find(".Product__OffScreen").html());
+        if($.trim($(data).find("#judgeme_product_reviews").html()) == "") {
+          $("#judgeme_product_reviews").html('<div class="jdgm-rev-widg jdgm--js" data-number-of-reviews="0" data-average-rating="0.00"><div class="jdgm-rev-widg__header"><h2 class="jdgm-rev-widg__title">WHAT PEPOLE SAYS ABOUT PRODUCT</h2><div class="jdgm-rev-widg__summary"><div class="jdgm-rev-widg__summary-stars"><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a></div><div class="jdgm-rev-widg__summary-text">No reviews yet</div></div><div class="jdgm-widget-actions-wrapper"> <a href="#" class="jdgm-write-rev-link">Write a review</a></div><div class="jdgm-rev__br"></div><div class="jdgm-form-wrapper" style="display: none;"><form class="jdgm-form" novalidate="novalidate"><div class="jdgm-form__name-fieldset"> <label for="jdgm_review_reviewer_name_5syzbea3">Name</label> <span class="jdgm-form__reviewer-name-format-container"> (<label for="jdgm_review_reviewer_name_format_5syzbea3" class="jdgm-form__inline-label jdgm-always-visible">displayed publicly like</label> <span class="jdgm-form__reviewer-name-format-dropdown-wrapper"> <select id="jdgm_review_reviewer_name_format_5syzbea3" name="reviewer_name_format" class="jdgm-form__reviewer-name-format-dropdown"><option value="" selected="">John Smith</option><option value="last_initial">John S.</option><option value="all_initials">J.S.</option><option value="anonymous">Anonymous</option> </select> <span class="jdgm-form__reviewer-name-format-dropdown-arrow"></span> </span> ) </span><input id="jdgm_review_reviewer_name_5syzbea3" name="reviewer_name" type="text" placeholder="Enter your name (public)" aria-label="Name"></div><div class="jdgm-form__email-fieldset"> <label for="jdgm_review_reviewer_email_5syzbea3">Email</label> <input id="jdgm_review_reviewer_email_5syzbea3" name="reviewer_email" type="email" required="" placeholder="Enter your email (private)" aria-label="Email" aria-required="true"></div><div class="jdgm-form__rating-fieldset" aria-label="Rating"> <label>Rating</label> <span class="jdgm-form__rating" style="cursor: pointer;"><a data-alt="1" class="jdgm-star jdgm--on" title="1 star" aria-label="1 star"></a><a data-alt="2" class="jdgm-star jdgm--on" title="2 stars" aria-label="2 stars"></a><a data-alt="3" class="jdgm-star jdgm--on" title="3 stars" aria-label="3 stars"></a><a data-alt="4" class="jdgm-star jdgm--on" title="4 stars" aria-label="4 stars"></a><a data-alt="5" class="jdgm-star jdgm--on" title="5 stars" tabindex="0" aria-label="5 stars"></a><input name="score" type="hidden" value="5"></span></div><div class="jdgm-form__title-fieldset"> <label for="jdgm_review_title_5syzbea3">Review Title</label><span class="jdgm-countdown"></span> <input id="jdgm_review_title_5syzbea3" name="review_title" type="text" placeholder="Give your review a title" aria-label="Review Title"></div><div class="jdgm-form__body-fieldset"> <label for="jdgm_review_body_5syzbea3">Review</label><span class="jdgm-countdown"></span><textarea id="jdgm_review_body_5syzbea3" rows="5" name="review_body" placeholder="Write your comments here" aria-label="Review"></textarea></div><div class="jdgm-custom-forms"></div><input type="submit" class="jdgm-submit-rev btn btn_c button " value="Submit Review"></form></div><div class="jdgm-form-dynamic-wrapper"><div class="jdgm-mask"></div><form class="jdgm-form-dynamic" novalidate="novalidate"><div class="jdgm-form-dynamic__close jdgm-close-ico"></div><div class="jdgm-form-dynamic__picture-upload-field jdgm-form-dynamic__row" data-pos="-1"><label class="jdgm-picture-dynamic-fieldset-title">Picture (optional)</label><div class="jdgm-form-dynamic__buttons-row"><div class="jdgm-form-dynamic__back btn btn_c button" tabindex="0">Back</div><div class="jdgm-form-dynamic__next btn btn_c button" tabindex="0">Next</div></div></div><div class="jdgm-form-dynamic__rating-fieldset jdgm-form-dynamic__row" data-pos="0"> <label>Rating</label> <span class="jdgm-form-dynamic__rating" style="cursor: pointer;"><a data-alt="1" class="jdgm-star jdgm--on" title="1 star"></a><a data-alt="2" class="jdgm-star jdgm--on" title="2 stars"></a><a data-alt="3" class="jdgm-star jdgm--on" title="3 stars"></a><a data-alt="4" class="jdgm-star jdgm--on" title="4 stars"></a><a data-alt="5" class="jdgm-star jdgm--on" title="5 stars"></a><input name="score" type="hidden" value="5"></span><div class="jdgm-form-dynamic__buttons-row"><div class="jdgm-form-dynamic__back btn btn_c button" tabindex="0">Back</div><div class="jdgm-form-dynamic__next btn btn_c button" tabindex="0">Next</div></div></div><div class="jdgm-form-dynamic__row" data-pos="1"><div class="jdgm-form-dynamic__name-fieldset"> <label for="jdgm_reviewer_name">Name</label> <input id="jdgm_reviewer_name" name="reviewer_name" type="text" placeholder="Enter your name (public)"></div><div class="jdgm-form-dynamic__email-fieldset"> <label for="jdgm_reviewer_email">Email</label> <input id="jdgm_reviewer_email" name="reviewer_email" type="email" placeholder="Enter your email (private)"></div><div class="jdgm-form-dynamic__buttons-row"><div class="jdgm-form-dynamic__back btn btn_c button" tabindex="0">Back</div><div class="jdgm-form-dynamic__next btn btn_c button" tabindex="0">Next</div></div></div><div class="jdgm-form-dynamic__row jdgm-form-dynamic__submit-slide" data-pos="2"><div class="jdgm-form-dynamic__title-fieldset"> <label for="jdgm_review_title">Review Title</label><span class="jdgm-countdown"></span> <input id="jdgm_review_title" name="review_title" type="text" placeholder="Give your review a title"></div><div class="jdgm-form-dynamic__body-fieldset"> <label for="jdgm_review_body">Review</label><span class="jdgm-countdown"></span><textarea id="jdgm_review_body" rows="5" name="review_body" placeholder="Write your comments here"></textarea></div><div class="jdgm-form-dynamic__buttons-row"><div class="jdgm-form-dynamic__back btn btn_c button" tabindex="0">Back</div><div class="jdgm-form-dynamic__next btn btn_c button" tabindex="0">Next</div></div></div><div class="jdgm-custom-forms-dynamic jdgm-custom-forms"></div><input type="submit" class="jdgm-form-dynamic__submit-rev btn btn_c button " value="Submit Review"></form></div></div></div>');
         }
-    }
+
+        history.pushState('data to be passed', 'Title of the page', _this.attr('href'));
+        if($.trim($(data).find(".Product__Info").find(".jdgm-widget.jdgm-preview-badge").html()) == "") {
+         $(".Product__Info").find(".jdgm-widget.jdgm-preview-badge").html('<div class="jdgm-prev-badge jdgm--js" data-average-rating="0.00" data-number-of-reviews="0"><span class="jdgm-prev-badge__stars" data-score="0.00"><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a><a class="jdgm-star jdgm--off"></a></span><span class="jdgm-prev-badge__text"> No reviews</span></div>');
+        }
+
+        $("#judgeme_product_reviews").attr("data-id", $(data).find("#judgeme_product_reviews").data("id"));
+        $("#judgeme_product_reviews").attr("data-product-title", $(data).find("#judgeme_product_reviews").data("product-title"));
+        // console.log(_this.closest(".Product__Info").find(".ProductMeta").html());
+        // console.log($(data).find(".Product__Info").html());    
+        console.log($("#judgeme_product_reviews").data("id"));
+      }
+    })
+    // if($(window).width() > 640) {
+    //   // event.preventDefault();
+    //   var _class = $(this).data('tooltip');
+    //   _class = "." + _class;
+    //   if($(this).parents(".Product__Wrapper").find(".product__media").find(_class).length != 0) {
+    //       $(this).parents(".Product__Wrapper").find(".product__media").find(".variant-item").hide();
+    //       $(this).parents(".Product__Wrapper").find(".product__media").find(_class).show();
+    //     }
+    // }
   })
   $(document).on("click", ".size-swatch", function(event){
     event.preventDefault();
     $(this).next(".size-list").slideToggle();
     $(this).toggleClass("open");
-    // $(".swathOverlay").show();
   })
   $(document).on("click", ".size-list .size-item", function(){
     var _button = $(this).parent(".size-list").prev("button.size-swatch");
